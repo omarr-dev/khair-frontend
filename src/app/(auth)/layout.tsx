657745e6bf800e15a7933379e9c59918b5e08022
اسمك الكامل <omarr9sulaiman@gmail.com>
2026-01-25 20:39:05 +0300

    f

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -4,7 +4,7 @@ export default function AuthLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="min-h-screen">
+    <div className="h-screen overflow-hidden">
       {children}
     </div>
   );
